753e324610188ca2b389f4e61215195040e88651
David Brochart <david.brochart@gmail.com>
2020-02-26 00:00:35 +0100

    Back to pychrono v4.0.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,5 @@
 sed -i -e '8d' setup.py
 
-if [ `uname` == Darwin ]; then
-    sed -i '' 's/ChSolverMINRES/ChSolverPMINRES/g' $SRC_DIR/proteus/mbd/*.h
-else
-    sed -i 's/ChSolverMINRES/ChSolverPMINRES/g' $SRC_DIR/proteus/mbd/*.h
-fi
-
 export PROTEUS_PREFIX=$PREFIX
 export PROTEUS_OPT=-I$PREFIX/include/eigen3
 
